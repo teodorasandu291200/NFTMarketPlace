491d4b83e3aaf759dd730179e5c9c4ba9afa9fe2
Teodora Sandu <86596578+teodorasandu291200@users.noreply.github.com>
2022-07-03 20:33:22 +0100

    Update _app.js
    Final Commit

--- pages/_app.js
+++ pages/_app.js
@@ -6,11 +6,12 @@ import { MoralisProvider } from "react-moralis"
 function MyApp({ Component, pageProps }) {
   return (
     <div>
-      <nav className="p-20 bg-black font-mono items-center justify-center ">
-        <MoralisProvider appId="K0O0qsZ1XJIaMxOwqR1Sx86I83eIvscCIp5g7IbY" serverUrl="https://6mnxcqddrcq0.usemoralis.com:2053/server">
-        <div className = "grid place-items-center  ">
-                    <Login/>
-                </div>
+      <nav className="p-20 bg-black font-mono ">
+      <MoralisProvider appId="K0O0qsZ1XJIaMxOwqR1Sx86I83eIvscCIp5g7IbY" serverUrl="https://6mnxcqddrcq0.usemoralis.com:2053/server">
+        <div className = "flex justify-end ">
+                    <Login
+                    />
+              </div>
         </MoralisProvider>
         <p className="text-4xl font-bold text-white flex-1 flex justify-center mr-auto">Metaverse NFT Market</p>
         <div className="flex mt-4 space-x-4"></div>

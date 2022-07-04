c54cef33ef0ec0580cf9ebd4b6440c90fc01c734
Teodora Sandu <86596578+teodorasandu291200@users.noreply.github.com>
2022-07-04 18:00:52 +0100

    Update NFT.sol

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -4,8 +4,20 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "./Auction.sol";
 
 contract NFT is ERC721URIStorage {
+
+    event AuctionCreated (
+        address indexed auctionAddress
+    );
+
+    address auctionAddress = address(0);
+
+    function getAuctionAddress() public view returns (address) {
+    return auctionAddress;
+}
+
 
     //auto-increment field for each token
     using Counters for Counters.Counter;
@@ -31,9 +43,21 @@ contract NFT is ERC721URIStorage {
         _mint(msg.sender, newItemId); //mint the token
         _setTokenURI(newItemId, tokenURI); //generate the URI
         setApprovalForAll(contractAddress, true); //grant transaction permission to marketplace
+        auctionAddress = deployAuction();
 
         //return token ID
         return newItemId;
 
     }
+
+    function deployAuction() public payable returns (address auction) {
+    address newAuction = address(new Auction(250, payable(msg.sender)));
+    emit AuctionCreated (
+        newAuction
+    );
+
+    return newAuction;
+
+
+  }
 }

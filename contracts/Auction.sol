8fbd7c39b19aac7db286c04d00f105b8c8f28f5a
Teodora Sandu <86596578+teodorasandu291200@users.noreply.github.com>
2022-07-04 18:00:40 +0100

    Update Auction.sol

--- contracts/Auction.sol
+++ contracts/Auction.sol
@@ -17,17 +17,18 @@ contract Auction {
     event HighestBidIncrease(address bidder, uint amount);
     event AuctionEnded(address winner, uint amount);
 
-    constructor(uint _biddingTime, address payable _beneficiary) {
+      constructor(uint _biddingTime, address payable _beneficiary) {
         beneficiary = _beneficiary;
         auctionEndTime= block.timestamp + _biddingTime;
     }
 
-    function bid() public payable {
+    function bid( uint price) public payable {
+        require(msg.sender != beneficiary, "Cannot bid on the same aucction you started");
         if ( block.timestamp > auctionEndTime ) {
             revert("The auction has ended");
         }
 
-        if ( msg.value <= highestBid) {
+        if ( price <= highestBid) {
             revert("Higher bidder existent");
         }
 
@@ -36,12 +37,13 @@ contract Auction {
         }
 
         highestBidder = msg.sender;
-        highestBid = msg.value;
-        emit HighestBidIncrease(msg.sender, msg.value);
+        highestBid = price;
+        emit HighestBidIncrease(msg.sender, price);
     }
 
 
     function withdraw() public returns (bool) {
+        require(msg.sender != highestBidder && msg.sender != beneficiary, "Cannot withdraw, you have the highest bid");
         uint amount = pendingReturns[msg.sender];
         if ( amount > 0 ) {
             pendingReturns[msg.sender] = 0;
@@ -54,9 +56,8 @@ contract Auction {
         return true;
     }
 
-
-
     function auctionEnd() public {
+        require(msg.sender == beneficiary, "Cannot end, you're not the beneficiary");
         if ( block.timestamp < auctionEndTime ) {
             revert("Auction not ended");
         }
@@ -67,8 +68,6 @@ contract Auction {
 
         ended = true;
         emit AuctionEnded(highestBidder, highestBid);
-
-        beneficiary.transfer(highestBid);
     }
 
 }
